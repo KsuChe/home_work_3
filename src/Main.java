f7c17d71eb9e08494d024a58ae153adf9181e06c
KsuChe <nerro@mail.ru>
2022-04-20 22:07:31 +0300

    task 1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,27 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
+     //task 1
+byte box = 45;
+short bananas = 445;
+int oranges = 4444445;
+long fruits = 4444444444444444445L;
+
+float fruitWeight = 4.5f;
+double vegetablesWeight = 4.5555555;
+
+char c = 45;
+boolean f = 30<45;
+
+
+
+
+
+
+
+
+
+
 
     }
 }
