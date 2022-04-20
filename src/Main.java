ef405b069ce5bf7dd8f4fbcc8404c9160ce69261
KsuChe <nerro@mail.ru>
2022-04-20 22:08:05 +0300

    task 2

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,14 @@ double vegetablesWeight = 4.5555555;
 
 char c = 45;
 boolean f = 30<45;
+// task 2
+        float firstBoxer = 78.2f;
+        float secondBoxer = 82.7f;
+        float totalWeight = firstBoxer + secondBoxer;
+        System.out.println("Общий вес бойцов " + totalWeight + " кг");
+        float weightDifference = secondBoxer - firstBoxer;
+        System.out.println("Разница в весе бойцов " + weightDifference + " кг");
+
 
 
 
