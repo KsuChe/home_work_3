8c282741338be870a09e0a5236bba8754fc92191
KsuChe <nerro@mail.ru>
2022-04-20 22:08:33 +0300

    task 3

--- src/Main.java
+++ src/Main.java
@@ -21,6 +21,26 @@ boolean f = 30<45;
         float weightDifference = secondBoxer - firstBoxer;
         System.out.println("Разница в весе бойцов " + weightDifference + " кг");
 
+// task 2
+        float firstBoxer = 78.2f;
+        float secondBoxer = 82.7f;
+        float totalWeight = firstBoxer + secondBoxer;
+        System.out.println("Общий вес бойцов " + totalWeight + " кг");
+        float weightDifference = secondBoxer - firstBoxer;
+        System.out.println("Разница в весе бойцов " + weightDifference + " кг");
+
+// task 3
+
+        int BananaWeight = 80 * 5;
+        int milkWeight = 105 * 2;
+        int iceCream = 100 * 2;
+        int egg = 70 * 4;
+        int productsWeight = BananaWeight + milkWeight + iceCream + egg;
+        System.out.println("Вес спорт-завтрака " + productsWeight + " гр");
+        int grPerKg = 1000;
+        float presentImKg = productsWeight / (float) grPerKg;
+        System.out.println(presentImKg + "кг");
+
 
 
 
